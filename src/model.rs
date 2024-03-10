a2114d67ce60acf5fd0c89f66b379cf127cc7c39
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2024-03-11 12:39:42 +1300

    create file and add boilerplate

---
+++ src/model.rs
@@ -0,0 +1,17 @@
+pub trait Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static>;
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct ModelVertex {
+    pub position: [f32; 3],
+    pub tex_coords: [f32; 3],
+    pub normal: [f32; 3],
+}
+
+impl Vertex for ModelVertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        todo!();
+    }
+}
